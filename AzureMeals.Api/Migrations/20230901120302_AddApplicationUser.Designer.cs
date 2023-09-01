19ac1ae83ba90ae4dc13112eabdcf2936082472e
rwaldron <outsideinprofits@gmail.com>
2023-09-01 22:05:17 +1000

    Add: Name field for user to AspNetUser table

--- AzureMeals.Api/Migrations/20230901120302_AddApplicationUser.Designer.cs
+++ AzureMeals.Api/Migrations/20230901120302_AddApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using AzureMeals.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AzureMeals.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230901120302_AddApplicationUser")]
+    partial class AddApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
