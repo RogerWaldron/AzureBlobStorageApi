d053afbcabc3d9662c7236953bd7dc4de3c0f28e
rwaldron <outsideinprofits@gmail.com>
2023-09-05 13:48:24 +1000

    feat: :bricks: Azure Blob Service Service and Repo

--- AzureMeals.Api/Services/IBlobService.cs
+++
@@ -1,9 +0,0 @@
-namespace AzureMeals.Api.Services
-{
-  public interface IBlobService
-  {
-    Task<string> GetBlob(string blobName, string containerName);
-    Task<string> UploadBlob(string blobName, string containerName, IFormFile file);
-    Task<bool> DeleteBlob(string blobName, string containerName);
-  }
-}
