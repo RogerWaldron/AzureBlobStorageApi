fa41d06762cac18d63cacf42988638c528a06fc8
rwaldron <outsideinprofits@gmail.com>
2023-09-05 13:45:27 +1000

    feat: :card_file_box: Azure Blob Service Models

---
+++ AzureMeals.Api/Models/Domain/AzureBlobResponse.cs
@@ -0,0 +1,15 @@
+namespace AzureMeals.Api.Models
+
+{
+    public class AzureBlobResponse
+    {
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public AzureBlob Blob { get; set; }
+
+        public AzureBlobResponse()
+        {
+            Blob = new AzureBlob();
+        }
+    }  
+}
