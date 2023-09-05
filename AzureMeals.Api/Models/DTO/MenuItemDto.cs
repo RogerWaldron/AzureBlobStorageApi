bc459f6580cd84d7c2b79aaa5d40adadac6c793b
rwaldron <outsideinprofits@gmail.com>
2023-09-05 13:54:30 +1000

    feat: :card_file_box: MenuItem Model and DTO's

--- AzureMeals.Api/Models/DTO/MenuItemDto.cs
+++ AzureMeals.Api/Models/DTO/MenuItemDto.cs
@@ -2,11 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AzureMeals.Api.Models
 {
-  public class MenuItemDto
+    public class MenuItemDto
   {
     public int Id { get; set;}
     public required string Name { get; set;}
     public required string Description { get; set;}
+    
+    [Range(1, int.MaxValue)]
     public double Price { get; set;}
     public string Category { get; set;} = "";
     public string Tag { get; set;} = "";
